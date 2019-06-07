252b35cea713b3b82d7dc10e1e8f8fbce4947931
Andrianina <andrianina@hairun-technology.com>
2019-06-07 16:03:37 +0300

    just use js

---
+++ pages/index.js
@@ -0,0 +1,12 @@
+import Link from 'next/link';
+import Layout from '../components/common/Layout';
+
+const Index = () => (
+  <div>
+    <Layout>
+      <p>Hello Next.js</p>
+    </Layout>
+  </div>
+);
+
+export default Index;
